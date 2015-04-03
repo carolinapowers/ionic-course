9c30a22ccb70963be9c64c457976bc85c0a2383a
Carolina Powers <carolinapoloni@gmail.com>
2015-04-02 20:36:45 -0400

    ionic loading icon and hideloading() on init function

--- code/www/js/controllers.js
+++ code/www/js/controllers.js
@@ -4,11 +4,28 @@ angular.module('songhop.controllers', ['ionic', 'songhop.services'])
 /*
 Controller for the discover page
 */
-.controller('DiscoverCtrl', function($scope,$timeout, User, Recommendations) {
-      Recommendations.init()
+.controller('DiscoverCtrl', function($scope,$timeout, User, Recommendations, $ionicLoading) {
+    // helper functions for loading
+  var showLoading = function() {
+    $ionicLoading.show({
+      template: '<i class="ion-loading-c"></i>',
+      noBackdrop: true
+    });
+  }
+
+  var hideLoading = function() {
+    $ionicLoading.hide();
+  }
+
+  // set loading to true first time while we retrieve songs from server.
+  showLoading();
+    
+    Recommendations.init()
     .then(function(){
       $scope.currentSong = Recommendations.queue[0];
-                Recommendations.playCurrentSong();
+      Recommendations.playCurrentSong();
+      
+      hideLoading();
     });
    
     /*$scope.songs = [
