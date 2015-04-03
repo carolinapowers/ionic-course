03e38eb4da2b09a8cb47f03e3516bcbe55f310ca
Carolina Powers <carolinapoloni@gmail.com>
2015-04-02 21:23:53 -0400

    we can now open favorite song in another window on click

--- code/www/js/controllers.js
+++ code/www/js/controllers.js
@@ -98,11 +98,14 @@ Controller for the discover page
 /*
 Controller for the favorites page
 */
-.controller('FavoritesCtrl', function($scope, User) {
+.controller('FavoritesCtrl', function($scope, $window, User) {
     $scope.favorites = User.favorites;
     
      $scope.removeSong = function(song, index) {
     User.removeSongFromFavorites(song, index);
+  }
+     $scope.openSong = function(song) {
+    $window.open(song.open_url, "_system");
   }
     
 
